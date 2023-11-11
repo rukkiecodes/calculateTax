596e9e2bbe900388bd4ce4daa80596a258093ed6
rukkiecodes <rukkiecodes@gmail.com>
2023-11-12 00:51:10 +0100

    made update to the code

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-export default (prop1, prop2, profile) => {
+export default (prop1, prop2, profile, addTax) => {
     return new Promise((resolve, reject) => {
-
-
         const baseAmount = parseFloat(prop1?.subTotal != undefined ? (typeof prop1?.subTotal == 'string' ? prop1?.subTotal.replace(/,/g, '') : prop1?.subTotal) : 0)
 
-
-        const tax = parseFloat(prop2?.tax) == 0 ? parseFloat(profile?.taxType?.tax) : parseFloat(prop2?.tax)
-
+        const tax = parseFloat(prop2?.tax) == 0 ? parseFloat(addTax ? profile?.taxType?.tax : 0) : parseFloat(addTax ? prop2?.tax : 0)
 
         const taxAmount = parseFloat(baseAmount) * (tax / 100);
 

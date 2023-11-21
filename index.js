4e7d382040ded63e483c096f36a559262ec8880b
rukkiecodes <rukkiecodes@gmail.com>
2023-11-21 17:46:09 +0100

    made update to tax variation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export default (prop1, prop2, profile, addTax) => {
     return new Promise((resolve, reject) => {
         const baseAmount = parseFloat(prop1?.subTotal != undefined ? (typeof prop1?.subTotal == 'string' ? prop1?.subTotal.replace(/,/g, '') : prop1?.subTotal) : 0)
 
-        const tax = parseFloat(prop2?.tax) == 0 ? parseFloat(addTax ? profile?.taxType?.tax : 0) : parseFloat(addTax ? prop2?.tax : 0)
+        const tax = addTax ? parseFloat(prop2?.tax) == 0 ? parseFloat(profile?.taxType?.tax) : parseFloat(prop2?.tax) : 0
 
         const taxAmount = parseFloat(baseAmount) * (tax / 100);
 
